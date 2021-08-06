acc1fbc4f6b6ef820e2387a5b2f2aa813f53b644
Kianush Dixon <kianushdixon@gmail.com>
2021-08-05 21:52:29 -0500

    passport...

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -46,28 +48,24 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 //Authenticating user...
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next){
-  console.log(req.session)
-  if (!req.session.user){
+  console.log(req.user)
+  if (!req.user){
      
       const err = new Error('You are not authenticated!');
       err.status = 401;
       return next(err);
 
-  } else{
-    if (req.session.user === 'authenticated'){
+  } else {
       return next();
-    } else {
-        const err = new Error('You still aint authenticated!');
-        err.status = 401;
-        return next(err);
-
-    }
   }
 
 }
